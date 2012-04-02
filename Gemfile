9853dd288a1f00dba52a11a06bd5cfbe07b8e89b
aimee daniells <aimeedaniells@googlemail.com>
2012-04-02 22:35:13 +0100

    Initialise SASS.

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source :rubygems
 
 gem 'sinatra', '1.3.2'
 gem 'haml', '3.1.4'
+gem 'sass', '3.1.15'
--- portfolio.rb
+++ portfolio.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 require 'sinatra'
 require 'haml'
+require 'sass'
 
 configure do
   set :haml, format: :html5
