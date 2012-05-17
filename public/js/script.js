f05dc591e4c987845be15ffa5ce28f856a34724e
aimee daniells <aimeedaniells@googlemail.com>
2012-05-17 23:04:37 +0100

    Hook up the nav click behaviour.

---
+++ public/js/script.js
@@ -0,0 +1,14 @@
+$.ready = function() {
+
+  $('#pet-projects').hide();
+
+  $('nav ul li a').live('click', function() {
+    $('nav ul li a').removeClass('active');
+    $(this).addClass('active');
+    $('.portfolio .section').hide();
+    var section = $(this).attr('data-section');
+    $('#' + section).show();
+    return false;
+  });
+
+}
