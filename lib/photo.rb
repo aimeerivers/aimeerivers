aa9fa17404b04df8d2ac036ffd130fdc4524c653
aimee daniells <aimeedaniells@googlemail.com>
2012-11-07 22:50:19 +0000

    Add width and height properties.

--- lib/photo.rb
+++ lib/photo.rb
@@ -16,11 +16,21 @@ class Photo
     @data.find_first('link').content
   end
 
+  def width
+    return @data.find_first('media:content')['width'].to_i
+  end
+
+  def height
+    return @data.find_first('media:content')['height'].to_i
+  end
+
   def to_hash
     {
       title: title,
       image: image,
-      link: link
+      link: link,
+      width: width,
+      height: height
     }
   end
 
