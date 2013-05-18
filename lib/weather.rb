71073c7bb62fab3929c9812ba004632d017b27fc
aimee rivers <aimee@aimeerivers.com>
2013-05-18 23:37:35 +0100

    Add temperature.

--- lib/weather.rb
+++ lib/weather.rb
@@ -1,10 +1,11 @@
 class Weather
-  attr_accessor :city, :description, :short_description
+  attr_accessor :city, :description, :short_description, :temperature
 
   def initialize(json)
     @city = json['name']
     @description = json['weather'][0]['description']
     @short_description = json['weather'][0]['main']
+    @temperature = json['main']['temp'].to_f - 273.15
   end
 
 end
